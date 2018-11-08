eaa3327100f36d351402e54213199ba89d8c3985
Greg McGrath <Greg.McGrath@contractor.cengage.com>
2018-11-08 15:03:26 -0500

    show all Messages when hovering over the imported object

--- src/HoverProvider.ts
+++ src/HoverProvider.ts
@@ -1,44 +1,52 @@
-import { HoverProvider, TextDocument, Position, CancellationToken, Hover, MarkedString } from "vscode";
-import * as path from "path";
 import {
-    getAllMessages,
-    getImportPathByPosition
+  HoverProvider,
+  TextDocument,
+  Position,
+  CancellationToken,
+  Hover,
+  MarkedString,
+} from "vscode";
+
+import {
+  getAllMessages,
+  getImportPathByWords,
+  getWordsAtPosition,
 } from "./utils";
 
-
-
 export class MessagesHoverProvider implements HoverProvider {
-    provideHover(document: TextDocument, position: Position, _token: CancellationToken): Thenable<Hover> {
-        const wordRange = document.getWordRangeAtPosition(position);
-        if (!wordRange) {
-            return Promise.resolve(null);
-        }
-        
-        const importPath = getImportPathByPosition(document, wordRange.end);
-        if (importPath === "") {
-            return Promise.resolve(null);
-        }
-        
-        const field = document.getText(wordRange);
-
-        const messages = getAllMessages(importPath, field);
-        if (messages) {
-            const contents: MarkedString[] = [
-                ...messages.reduce((arr, message) => ([
-                    ...arr,
-                    "Text: ",
-                    ` - "${message.defaultMessage}"`,
-                    ...(message.description !== undefined ? ['Description:', ` - ${message.description}`] : [])
-                ]), [])
-            ];
-
-            const hover = new Hover(contents, wordRange);
-
-            return Promise.resolve(hover)
-        }
-
-        return Promise.resolve(null);
+  provideHover(
+    document: TextDocument,
+    position: Position,
+    _token: CancellationToken,
+  ): Thenable<Hover> {
+    const wordRange = document.getWordRangeAtPosition(position);
+    if (!wordRange) {
+      return Promise.resolve(null);
     }
+
+    const words = getWordsAtPosition(document, wordRange.end);
+
+    const importPath = getImportPathByWords(document, words);
+    if (importPath === null) {
+      return Promise.resolve(null);
+    }
+
+    const field = document.getText(wordRange);
+    const isFieldFiltered = field !== words;
+
+    const messages = getAllMessages(importPath, isFieldFiltered ? field : "");
+
+    if (!messages) {
+      return Promise.resolve(null);
+    }
+
+    const contents: MarkedString[] = [
+      { value: JSON.stringify(messages, null, 2), language: "json" },
+    ];
+    const hover = new Hover(contents, wordRange);
+
+    return Promise.resolve(hover);
+  }
 }
 
 export default MessagesHoverProvider;
